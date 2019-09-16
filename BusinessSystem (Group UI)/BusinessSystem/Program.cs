10bbbe1e4b3609541ec0ae98f08dbe87fb83ce10
Rory Chang <fuxk280843@gmail.com>
2019-09-16 20:05:49 +0800

    new
    new

---
+++ BusinessSystem (Group UI)/BusinessSystem/Program.cs
@@ -0,0 +1,25 @@
+﻿using BusinessSystem.ReportTimeSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BusinessSystem
+{
+    static class Program
+    {
+        /// <summary>
+        /// 應用程式的主要進入點。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new FormAdd ());
+            Application.Run(new CreateEmployeeForm());
+        }
+    }
+}
