651e6f5cf31a7ece38c58e2cd031f61330b9ed62
吳晨瑋 <54974572+jenniferwu840209@users.noreply.github.com>
2019-09-16 09:51:01 +0800

    修改mainform
    修改mainform

--- BusinessSystem (Group UI)/BusinessSystem/Program.cs
+++ BusinessSystem (Group UI)/BusinessSystem/Program.cs
@@ -18,6 +18,7 @@ namespace BusinessSystem
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new BullitinBoard_2());
+            Application.Run(new MainForm());
         }
     }
 }
