6c12f0d506c16134e0e3cb635f44e92ea30f8803
kevinsean999 <qwe820321@gmail.com>
2019-09-16 11:10:59 +0800

    CompanyVehicle新增表單
    CompanyVehicle新增表單CompanyCarForm.

--- BusinessSystem (Group UI)/BusinessSystem/Program.cs
+++ BusinessSystem (Group UI)/BusinessSystem/Program.cs
@@ -18,6 +18,7 @@ namespace BusinessSystem
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
+            //Application.Run(new CompanyCarForm());
         }
     }
 }
