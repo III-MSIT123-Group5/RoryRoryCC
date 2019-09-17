539f81606183e095cabb0b4dd636701aa74471b3
jocelyn6065 <jocelyn6065@gmail.com>
2019-09-17 15:23:16 +0800

    Jocelyn
    Jocelyn

---
+++ BusinessSystem (Group UI)/BusinessSystem/CreateEmployeeForm.cs
@@ -0,0 +1,41 @@
+﻿using BusinessSystemDBEntityModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BusinessSystem
+{
+    public partial class CreateEmployeeForm : SonForm 
+    {
+        public CreateEmployeeForm()
+        {
+            InitializeComponent();
+            
+        }
+
+        BusinessDataBaseEntities dbcontext;
+
+        private void CreateEmployeeForm_Load(object sender, EventArgs e)
+        {
+            dbcontext = new BusinessDataBaseEntities();
+            //預設Office combobox內容 
+            this.cmbOfficeID.DataSource = dbcontext.Offices.OrderBy  (p=>p.officeID).Select(p => p.office_name ).Distinct ().ToList();
+            this.cmbOfficeID.SelectedIndex = 1;  // index 0 為Branch office ID應輸入 2，index 1 為Head office ID應輸入 1
+            //預設Department combobox內容 // if index==0 , 值為總經理，departmentID應輸入 index+1 累推
+            this.cmbDepartmentID.DataSource = dbcontext.Departments.OrderBy(p => p.departmentID).Select(p => p.name).ToList();
+            //預設Employed combobox內容
+            this.cmbEmployed.SelectedIndex = 1; // index 1 為"在職" 應輸入 1，index 0 為"離職" 應輸入 0
+            //預設Position ID內容 // if index==0 , 值為General manager，positionID應輸入 index+1 累推
+            this.cmbPositionID.DataSource = dbcontext.Positions.OrderBy(p => p.positionID).Select(p => p.position1).ToList();
+
+
+
+        }
+    }
+}
---
+++ BusinessSystem (Group UI)/BusinessSystem/EmployeeSystem/Login.designer.cs
@@ -0,0 +1,145 @@
+﻿namespace BusinessSystem
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.clsAltoButton1 = new BusinessSystem.ClsAltoButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // clsAltoButton1
+            // 
+            this.clsAltoButton1.Active1 = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(168)))), ((int)(((byte)(183)))));
+            this.clsAltoButton1.Active2 = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(164)))), ((int)(((byte)(183)))));
+            this.clsAltoButton1.BackColor = System.Drawing.Color.Transparent;
+            this.clsAltoButton1.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.clsAltoButton1.Font = new System.Drawing.Font("Comic Sans MS", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clsAltoButton1.ForeColor = System.Drawing.Color.White;
+            this.clsAltoButton1.Inactive1 = System.Drawing.Color.DarkSlateBlue;
+            this.clsAltoButton1.Inactive2 = System.Drawing.Color.DarkSlateBlue;
+            this.clsAltoButton1.Location = new System.Drawing.Point(243, 325);
+            this.clsAltoButton1.Name = "clsAltoButton1";
+            this.clsAltoButton1.Radius = 10;
+            this.clsAltoButton1.Size = new System.Drawing.Size(375, 66);
+            this.clsAltoButton1.Stroke = false;
+            this.clsAltoButton1.StrokeColor = System.Drawing.Color.Gray;
+            this.clsAltoButton1.TabIndex = 7;
+            this.clsAltoButton1.Text = "Login";
+            this.clsAltoButton1.Transparency = false;
+            this.clsAltoButton1.Click += new System.EventHandler(this.clsAltoButton1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DarkSlateBlue;
+            this.label2.Location = new System.Drawing.Point(257, 179);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 29);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Username :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.DarkSlateBlue;
+            this.label3.Location = new System.Drawing.Point(262, 235);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 29);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Password :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(398, 179);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(220, 30);
+            this.textBox1.TabIndex = 10;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(398, 239);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(220, 30);
+            this.textBox2.TabIndex = 12;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(77, 153);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(128, 128);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 13;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.clsAltoButton1);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.Controls.SetChildIndex(this.clsAltoButton1, 0);
+            this.Controls.SetChildIndex(this.label2, 0);
+            this.Controls.SetChildIndex(this.label3, 0);
+            this.Controls.SetChildIndex(this.textBox1, 0);
+            this.Controls.SetChildIndex(this.textBox2, 0);
+            this.Controls.SetChildIndex(this.pictureBox2, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private ClsAltoButton clsAltoButton1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
