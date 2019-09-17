7c96e258b674d5aa13bb3ff81f0f1b74d03b8f44
黃詠聖 <yongshengcard@gmail.com>
2019-09-17 10:46:08 +0800

    還原刪除的檔案
    還原刪除的檔案

--- BusinessSystem (Group UI)/BusinessSystem/Program.cs
+++ BusinessSystem (Group UI)/BusinessSystem/Program.cs
@@ -18,9 +18,9 @@ namespace BusinessSystem
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
-            Application.Run(new BulletinBoard_2());
-            Application.Run(new CompanyCarForm());
-            Application.Run(new CreateEmployeeForm ());
+            //Application.Run(new BulletinBoard_2());
+            //Application.Run(new CompanyCarForm());
+            //Application.Run(new CreateEmployeeForm ());
         }
     }
 }
---
+++ BusinessSystem (Group UI)/BusinessSystem/ReportTimeSystem/FormAdd.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.Entity.Core.EntityClient;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BusinessSystemDBEntityModel;
+
+
+
+namespace BusinessSystem.ReportTimeSystem
+{
+    public partial class FormAdd : SonForm
+    {
+        BusinessDataBaseEntities dbcontext = new BusinessDataBaseEntities();
+        BusinessSystemDBEntityModel.ReportTimeSystem rts = new BusinessSystemDBEntityModel.ReportTimeSystem();
+
+      
+        public FormAdd()
+        {
+            InitializeComponent();
+           
+            
+        }
+        
+        private void clsAltoButton1_Click(object sender, EventArgs e)
+        {
+          
+
+            dbcontext.ReportTimeSystems.Add(
+                new BusinessSystemDBEntityModel.ReportTimeSystem
+                {
+                    employeeID = 0,
+                    ApplyDateTime = DateTime.Now,
+                    EventHours = int.Parse(comboBox2.Text),
+                    EventID = FindID(comboBox1.Text),
+                    Note = richTextBox1.Text,
+                    Discontinue = true
+                });
+             
+           
+            dbcontext.SaveChanges();
+            
+        }
+
+        private int FindID(string Text)
+        {
+            int result=0;
+
+            if (dbcontext.Events.First().EventName == Text)
+            {
+                result = dbcontext.Events.First().EventID;
+            } 
+            return result;
+           
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FormAdd_Load(object sender, EventArgs e)
+        {
+            comboBox1.DataSource = dbcontext.Events.Select(n=>n.EventName).Distinct().ToList();
+           
+        }
+    }
+}
