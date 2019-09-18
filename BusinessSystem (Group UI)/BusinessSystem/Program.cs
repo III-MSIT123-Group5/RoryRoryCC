83c850835030f1bdf9eac8e8084af3460852934e
Rory Chang <fuxk280843@gmail.com>
2019-09-18 16:41:52 +0800

    update
    update

--- BusinessSystem (Group UI)/BusinessSystem/Program.cs
+++ BusinessSystem (Group UI)/BusinessSystem/Program.cs
@@ -20,8 +20,8 @@ namespace BusinessSystem
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            Application.Run(new MainForm());
-            Application.Run(new MainForm());
+            Application.Run(new MainForm  ());
+            
 
         }
     }
