2661b957535681f75eb27fda72fdca140ef9c58a
jocelyn6065 <jocelyn6065@gmail.com>
2019-09-19 17:11:23 +0800

    a little update
    a little update

--- BusinessSystem (Group UI)/BusinessSystem/Program.cs
+++ BusinessSystem (Group UI)/BusinessSystem/Program.cs
@@ -20,7 +20,7 @@ namespace BusinessSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());            
+            Application.Run(new LoginForm());            
         }
     }
 }
