f884a8159bbca8187a3aa883e5382fc8f698558d
kevinsean999 <qwe820321@gmail.com>
2019-09-19 11:44:34 +0800

    Kai 0919 11:50 first push
    Kai 0919 11:50 first push

---
+++ BusinessSystem (Group UI)/BusinessSystem/CompanyCars/ChangeVehicleHistory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BusinessSystem.CompanyCars
+{
+    public partial class ChangeVehicleHistory : SonForm
+    {
+        public ChangeVehicleHistory()
+        {
+            InitializeComponent();
+        }
+    }
+}
