78027f95b7580087c578c0b19bf0c1fd2019dc05
黃詠聖 <yongshengcard@gmail.com>
2019-09-21 17:15:46 +0800

    update chart and adjust data

--- BusinessSystem (Group UI)/BusinessSystem/Program.cs
+++ BusinessSystem (Group UI)/BusinessSystem/Program.cs
@@ -24,7 +24,7 @@ namespace BusinessSystem
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new LoginForm());
-            Application.Run(new CreateEmployeeForm());
+            Application.Run(new FormMainRTS());
         }
     }
 }
