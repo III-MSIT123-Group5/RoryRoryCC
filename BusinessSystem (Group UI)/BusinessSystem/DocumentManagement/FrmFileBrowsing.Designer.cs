1f84772b2fc173303ea76933d196b587f605faf9
Yvette-Ma <55129864+Yvette-Ma@users.noreply.github.com>
2019-09-22 15:28:01 +0800

    文件管理-條件搜索

--- BusinessSystem (Group UI)/BusinessSystem/DocumentManagement/FrmFileBrowsing.Designer.cs
+++ BusinessSystem (Group UI)/BusinessSystem/DocumentManagement/FrmFileBrowsing.Designer.cs
@@ -34,7 +34,20 @@
             this.clsAltoButton3 = new BusinessSystem.ClsAltoButton();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.clsAltoButton4 = new BusinessSystem.ClsAltoButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.clsAltoButton7 = new BusinessSystem.ClsAltoButton();
+            this.clsAltoButton5 = new BusinessSystem.ClsAltoButton();
+            this.clsAltoButton6 = new BusinessSystem.ClsAltoButton();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.clsAltoButton8 = new BusinessSystem.ClsAltoButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.clsAltoButton9 = new BusinessSystem.ClsAltoButton();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // clsAltoButton1
@@ -64,8 +77,9 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(163, 51);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToDisplayedHeaders;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(637, 399);
+            this.dataGridView1.Size = new System.Drawing.Size(637, 380);
             this.dataGridView1.TabIndex = 2;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
@@ -79,7 +93,7 @@
             this.clsAltoButton2.ForeColor = System.Drawing.Color.White;
             this.clsAltoButton2.Inactive1 = System.Drawing.Color.DarkSlateBlue;
             this.clsAltoButton2.Inactive2 = System.Drawing.Color.DarkSlateBlue;
-            this.clsAltoButton2.Location = new System.Drawing.Point(25, 155);
+            this.clsAltoButton2.Location = new System.Drawing.Point(25, 151);
             this.clsAltoButton2.Name = "clsAltoButton2";
             this.clsAltoButton2.Radius = 10;
             this.clsAltoButton2.Size = new System.Drawing.Size(105, 31);
@@ -100,7 +114,7 @@
             this.clsAltoButton3.ForeColor = System.Drawing.Color.White;
             this.clsAltoButton3.Inactive1 = System.Drawing.Color.DarkGray;
             this.clsAltoButton3.Inactive2 = System.Drawing.Color.DarkGray;
-            this.clsAltoButton3.Location = new System.Drawing.Point(25, 280);
+            this.clsAltoButton3.Location = new System.Drawing.Point(25, 328);
             this.clsAltoButton3.Name = "clsAltoButton3";
             this.clsAltoButton3.Radius = 10;
             this.clsAltoButton3.Size = new System.Drawing.Size(105, 31);
@@ -115,11 +129,191 @@
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
+            // clsAltoButton4
+            // 
+            this.clsAltoButton4.Active1 = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(168)))), ((int)(((byte)(183)))));
+            this.clsAltoButton4.Active2 = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(164)))), ((int)(((byte)(183)))));
+            this.clsAltoButton4.BackColor = System.Drawing.Color.Transparent;
+            this.clsAltoButton4.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.clsAltoButton4.Font = new System.Drawing.Font("微軟正黑體", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.clsAltoButton4.ForeColor = System.Drawing.Color.White;
+            this.clsAltoButton4.Inactive1 = System.Drawing.Color.DarkSlateBlue;
+            this.clsAltoButton4.Inactive2 = System.Drawing.Color.DarkSlateBlue;
+            this.clsAltoButton4.Location = new System.Drawing.Point(25, 210);
+            this.clsAltoButton4.Name = "clsAltoButton4";
+            this.clsAltoButton4.Radius = 10;
+            this.clsAltoButton4.Size = new System.Drawing.Size(105, 31);
+            this.clsAltoButton4.Stroke = false;
+            this.clsAltoButton4.StrokeColor = System.Drawing.Color.Gray;
+            this.clsAltoButton4.TabIndex = 5;
+            this.clsAltoButton4.Text = "條件查詢";
+            this.clsAltoButton4.Transparency = false;
+            this.clsAltoButton4.Click += new System.EventHandler(this.clsAltoButton4_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.clsAltoButton7);
+            this.panel1.Controls.Add(this.clsAltoButton5);
+            this.panel1.Controls.Add(this.clsAltoButton6);
+            this.panel1.Location = new System.Drawing.Point(2, 51);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(155, 256);
+            this.panel1.TabIndex = 6;
+            // 
+            // clsAltoButton7
+            // 
+            this.clsAltoButton7.Active1 = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(168)))), ((int)(((byte)(183)))));
+            this.clsAltoButton7.Active2 = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(164)))), ((int)(((byte)(183)))));
+            this.clsAltoButton7.BackColor = System.Drawing.Color.Transparent;
+            this.clsAltoButton7.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.clsAltoButton7.Font = new System.Drawing.Font("微軟正黑體", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.clsAltoButton7.ForeColor = System.Drawing.Color.White;
+            this.clsAltoButton7.Inactive1 = System.Drawing.Color.DarkSlateBlue;
+            this.clsAltoButton7.Inactive2 = System.Drawing.Color.DarkSlateBlue;
+            this.clsAltoButton7.Location = new System.Drawing.Point(23, 41);
+            this.clsAltoButton7.Name = "clsAltoButton7";
+            this.clsAltoButton7.Radius = 10;
+            this.clsAltoButton7.Size = new System.Drawing.Size(105, 31);
+            this.clsAltoButton7.Stroke = false;
+            this.clsAltoButton7.StrokeColor = System.Drawing.Color.Gray;
+            this.clsAltoButton7.TabIndex = 7;
+            this.clsAltoButton7.TabStop = false;
+            this.clsAltoButton7.Text = "檔名";
+            this.clsAltoButton7.Transparency = false;
+            this.clsAltoButton7.Click += new System.EventHandler(this.clsAltoButton7_Click);
+            // 
+            // clsAltoButton5
+            // 
+            this.clsAltoButton5.Active1 = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(168)))), ((int)(((byte)(183)))));
+            this.clsAltoButton5.Active2 = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(164)))), ((int)(((byte)(183)))));
+            this.clsAltoButton5.BackColor = System.Drawing.Color.Transparent;
+            this.clsAltoButton5.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.clsAltoButton5.Font = new System.Drawing.Font("微軟正黑體", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.clsAltoButton5.ForeColor = System.Drawing.Color.White;
+            this.clsAltoButton5.Inactive1 = System.Drawing.Color.DarkSlateBlue;
+            this.clsAltoButton5.Inactive2 = System.Drawing.Color.DarkSlateBlue;
+            this.clsAltoButton5.Location = new System.Drawing.Point(23, 159);
+            this.clsAltoButton5.Name = "clsAltoButton5";
+            this.clsAltoButton5.Radius = 10;
+            this.clsAltoButton5.Size = new System.Drawing.Size(105, 31);
+            this.clsAltoButton5.Stroke = false;
+            this.clsAltoButton5.StrokeColor = System.Drawing.Color.Gray;
+            this.clsAltoButton5.TabIndex = 9;
+            this.clsAltoButton5.TabStop = false;
+            this.clsAltoButton5.Text = "日期";
+            this.clsAltoButton5.Transparency = false;
+            this.clsAltoButton5.Click += new System.EventHandler(this.clsAltoButton5_Click);
+            // 
+            // clsAltoButton6
+            // 
+            this.clsAltoButton6.Active1 = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(168)))), ((int)(((byte)(183)))));
+            this.clsAltoButton6.Active2 = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(164)))), ((int)(((byte)(183)))));
+            this.clsAltoButton6.BackColor = System.Drawing.Color.Transparent;
+            this.clsAltoButton6.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.clsAltoButton6.Font = new System.Drawing.Font("微軟正黑體", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.clsAltoButton6.ForeColor = System.Drawing.Color.White;
+            this.clsAltoButton6.Inactive1 = System.Drawing.Color.DarkSlateBlue;
+            this.clsAltoButton6.Inactive2 = System.Drawing.Color.DarkSlateBlue;
+            this.clsAltoButton6.Location = new System.Drawing.Point(23, 100);
+            this.clsAltoButton6.Name = "clsAltoButton6";
+            this.clsAltoButton6.Radius = 10;
+            this.clsAltoButton6.Size = new System.Drawing.Size(105, 31);
+            this.clsAltoButton6.Stroke = false;
+            this.clsAltoButton6.StrokeColor = System.Drawing.Color.Gray;
+            this.clsAltoButton6.TabIndex = 8;
+            this.clsAltoButton6.TabStop = false;
+            this.clsAltoButton6.Text = "員工";
+            this.clsAltoButton6.Transparency = false;
+            this.clsAltoButton6.Click += new System.EventHandler(this.clsAltoButton6_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.dateTimePicker1);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.clsAltoButton8);
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Location = new System.Drawing.Point(2, 54);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(155, 150);
+            this.panel2.TabIndex = 10;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(23, 59);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(105, 22);
+            this.dateTimePicker1.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(10, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "檔案名稱：";
+            // 
+            // clsAltoButton8
+            // 
+            this.clsAltoButton8.Active1 = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(168)))), ((int)(((byte)(183)))));
+            this.clsAltoButton8.Active2 = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(164)))), ((int)(((byte)(183)))));
+            this.clsAltoButton8.BackColor = System.Drawing.Color.Transparent;
+            this.clsAltoButton8.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.clsAltoButton8.Font = new System.Drawing.Font("微軟正黑體", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.clsAltoButton8.ForeColor = System.Drawing.Color.White;
+            this.clsAltoButton8.Inactive1 = System.Drawing.Color.DarkSlateBlue;
+            this.clsAltoButton8.Inactive2 = System.Drawing.Color.DarkSlateBlue;
+            this.clsAltoButton8.Location = new System.Drawing.Point(23, 97);
+            this.clsAltoButton8.Name = "clsAltoButton8";
+            this.clsAltoButton8.Radius = 10;
+            this.clsAltoButton8.Size = new System.Drawing.Size(105, 31);
+            this.clsAltoButton8.Stroke = false;
+            this.clsAltoButton8.StrokeColor = System.Drawing.Color.Gray;
+            this.clsAltoButton8.TabIndex = 10;
+            this.clsAltoButton8.TabStop = false;
+            this.clsAltoButton8.Text = "查詢";
+            this.clsAltoButton8.Transparency = false;
+            this.clsAltoButton8.Click += new System.EventHandler(this.clsAltoButton8_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(23, 59);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(105, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // clsAltoButton9
+            // 
+            this.clsAltoButton9.Active1 = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(168)))), ((int)(((byte)(183)))));
+            this.clsAltoButton9.Active2 = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(164)))), ((int)(((byte)(183)))));
+            this.clsAltoButton9.BackColor = System.Drawing.Color.Transparent;
+            this.clsAltoButton9.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.clsAltoButton9.Font = new System.Drawing.Font("微軟正黑體", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.clsAltoButton9.ForeColor = System.Drawing.Color.White;
+            this.clsAltoButton9.Inactive1 = System.Drawing.Color.DarkSlateBlue;
+            this.clsAltoButton9.Inactive2 = System.Drawing.Color.DarkSlateBlue;
+            this.clsAltoButton9.Location = new System.Drawing.Point(25, 269);
+            this.clsAltoButton9.Name = "clsAltoButton9";
+            this.clsAltoButton9.Radius = 10;
+            this.clsAltoButton9.Size = new System.Drawing.Size(105, 31);
+            this.clsAltoButton9.Stroke = false;
+            this.clsAltoButton9.StrokeColor = System.Drawing.Color.Gray;
+            this.clsAltoButton9.TabIndex = 11;
+            this.clsAltoButton9.Text = "顯示全部文件";
+            this.clsAltoButton9.Transparency = false;
+            this.clsAltoButton9.Click += new System.EventHandler(this.clsAltoButton9_Click);
+            // 
             // FrmFileBrowsing
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.clsAltoButton9);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.clsAltoButton4);
             this.Controls.Add(this.clsAltoButton3);
             this.Controls.Add(this.clsAltoButton2);
             this.Controls.Add(this.dataGridView1);
@@ -129,6 +323,9 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "文件管理";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -141,5 +338,16 @@
         private ClsAltoButton clsAltoButton3;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private ClsAltoButton clsAltoButton4;
+        private System.Windows.Forms.Panel panel1;
+        private ClsAltoButton clsAltoButton5;
+        private ClsAltoButton clsAltoButton7;
+        private ClsAltoButton clsAltoButton6;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private ClsAltoButton clsAltoButton8;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private ClsAltoButton clsAltoButton9;
     }
 }
