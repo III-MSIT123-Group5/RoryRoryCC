62b943528081a6347007ad8db077decfa76efccc
吳晨瑋 <54974572+jenniferwu840209@users.noreply.github.com>
2019-09-22 10:54:13 +0800

    等待鍵
    20190922

--- BusinessSystem (Group UI)/BusinessSystem/EmployeeSystem/LoginForm.cs
+++ BusinessSystem (Group UI)/BusinessSystem/EmployeeSystem/LoginForm.cs
@@ -18,10 +18,9 @@ namespace BusinessSystem.EmployeeSystem
         {
             InitializeComponent();
         }
-        
+
         private void LoginForm_Load(object sender, EventArgs e)
         {
-
 
         }
 
@@ -30,59 +29,56 @@ namespace BusinessSystem.EmployeeSystem
 
         private void clsAltoButton1_Click(object sender, EventArgs e)
         {
-
-            BusinessDataBaseEntities dbContext;
-
-            dbContext = new BusinessDataBaseEntities();
-
-            byte[] bytesPassword = Encoding.Unicode.GetBytes(this.txtLoginPassword.Text);
-            SHA256Managed Algorithm = new SHA256Managed();
-            byte[] validPassword = Algorithm.ComputeHash(bytesPassword);
-
-
-
-
-
-            var q = from em in dbContext.Employees
-                    join a in dbContext.Accounts
-                    on em.Account equals a.account1
-                    where em.Account == this.txtLoginAccount.Text && a.password == validPassword
-                    select new { a.account1, a.password, em.employeeID, em.EmployeeName };
-
-            foreach (var v in q)
+            this.Cursor = Cursors.WaitCursor;
+            try
             {
-                EmpID = v.employeeID;
-                EmpName = v.EmployeeName;
-            }
+                BusinessDataBaseEntities dbContext;
 
-            //if (txtLoginAccount.Text == null && txtLoginPassword.Text == null)
-            //{
-            //    MessageBox.Show("請先輸入帳號及密碼！", "帳號及密碼不可為空", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
-            //}
-            
-            
-            if (q.Any())
+                dbContext = new BusinessDataBaseEntities();
+
+                byte[] bytesPassword = Encoding.Unicode.GetBytes(this.txtLoginPassword.Text);
+                SHA256Managed Algorithm = new SHA256Managed();
+                byte[] validPassword = Algorithm.ComputeHash(bytesPassword);
+
+                var q = from em in dbContext.Employees
+                        join a in dbContext.Accounts
+                        on em.Account equals a.account1
+                        where em.Account == this.txtLoginAccount.Text && a.password == validPassword
+                        select new { a.account1, a.password, em.employeeID, em.EmployeeName };
+
+                foreach (var v in q)
+                {
+                    EmpID = v.employeeID;
+                    EmpName = v.EmployeeName;
+                }
+
+                //if (txtLoginAccount.Text == null && txtLoginPassword.Text == null)
+                //{
+                //    MessageBox.Show("請先輸入帳號及密碼！", "帳號及密碼不可為空", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                //}
+
+                if (q.Any())
+                {
+                    //readname.EmpAcount = this.txtLoginAccount.Text;//擷取登入者姓名 (Kai)
+
+                    MessageBox.Show($"歡迎回來， {EmpName.Substring(EmpName.Length - 2, EmpName.Length - 1)}!", "登入成功", MessageBoxButtons.OK);
+
+                    //MessageBox.Show(readname.EmpAcount);//測試
+                    MainForm main = new MainForm(EmpID);
+                    main.Show();
+                }
+                else
+                {
+                    MessageBox.Show($"請重新登入", "登入失敗", MessageBoxButtons.OK);
+                    this.txtLoginPassword.Text = "";
+                    this.txtLoginPassword.Focus();
+                }
+            }
+            finally
             {
-
-                //readname.EmpAcount = this.txtLoginAccount.Text;//擷取登入者姓名 (Kai)
-                
-                MessageBox.Show($"歡迎回來， {EmpName.Substring (EmpName.Length -2,EmpName.Length-1 )}!", "登入成功", MessageBoxButtons.OK);
-               
-                //MessageBox.Show(readname.EmpAcount);//測試
-                MainForm main = new MainForm(EmpID);
-                main.Show();
+                this.Cursor = Cursors.Default;
             }
-            else
-            {
-
-                MessageBox.Show($"請重新登入", "登入失敗", MessageBoxButtons.OK);
-                this.txtLoginPassword.Text = "";
-                this.txtLoginPassword.Focus();
-            }
-
-
         }
-
 
         private void label5_Click(object sender, EventArgs e)
         {
--- BusinessSystem (Group UI)/BusinessSystem/Requisition/FrmRequisition(add,show).designer.cs
+++ BusinessSystem (Group UI)/BusinessSystem/Requisition/FrmRequisition(add,show).designer.cs
@@ -243,6 +243,7 @@
             // 
             // FrmRequisition1
             // 
+            this.AcceptButton = this.btnSubmit;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(901, 454);
