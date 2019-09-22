b36874314507dbec4d4f2d4c114707c3673baa6b
kevinsean999 <qwe820321@gmail.com>
2019-09-22 11:42:05 +0800

    Kai 0922 push1
    Kai 0922 push1

--- BusinessSystem (Group UI)/BusinessSystem/SonForm.cs
+++ BusinessSystem (Group UI)/BusinessSystem/SonForm.cs
@@ -14,61 +14,61 @@ namespace BusinessSystem
 {
     public partial class SonForm : Form
     {
-        //int EmpID;
-        //private int M_LoginID;
-        //public int LoginID
-        //{
-        //    get
-        //    {
-        //        return M_LoginID;
-        //    }
-        //    set
-        //    {
-        //        M_LoginID = value;
-        //    }
-        //}
-        //public SonForm()
-        //{
-        //    InitializeComponent();
-        //}
-        //public SonForm(int empID)
-        //{
-        //    InitializeComponent();
-        //    LoginID = empID;
-        //}
-        //string name = null;
-        //string gender = null;
-        //string photo = null;
-        
-       
-        //private void SonForm_Load(object sender, EventArgs e)
-        //{
-        //    BusinessDataBaseEntities dbContext;           
-        //    dbContext = new BusinessDataBaseEntities();
-        //    var q = from em in dbContext.Employees
-        //            where  em.employeeID  == LoginID  
-        //            select new { em.EmployeeName, em.Gender, em.Photo };
+        int EmpID;
+        private int M_LoginID;
+        public int LoginID
+        {
+            get
+            {
+                return M_LoginID;
+            }
+            set
+            {
+                M_LoginID = value;
+            }
+        }
+        public SonForm()
+        {
+            InitializeComponent();
+        }
+        public SonForm(int empID)
+        {
+            InitializeComponent();
+            LoginID = empID;
+        }
+        string name = null;
+        string gender = null;
+        string photo = null;
 
-        //    foreach (var n in q)
-        //    {
-        //        name = n.EmployeeName;
-        //        gender = n.Gender;
-        //        photo = n.Photo;
-        //    }
 
-        //    label3.Text = name;
+        private void SonForm_Load(object sender, EventArgs e)
+        {
+            BusinessDataBaseEntities dbContext;
+            dbContext = new BusinessDataBaseEntities();
+            var q = from em in dbContext.Employees
+                    where em.employeeID == LoginID
+                    select new { em.EmployeeName, em.Gender, em.Photo };
 
-        //    if (gender == "M")
-        //    {
-        //        label4.Text = "先 生";
-        //    }
-        //    else
-        //    {
-        //        label4.Text = "小 姐";
-        //    }
+            foreach (var n in q)
+            {
+                name = n.EmployeeName;
+                gender = n.Gender;
+                photo = n.Photo;
+            }
 
-        //    pictureBox2.ImageLocation = photo;
-        //}
+            label3.Text = name;
+
+            if (gender == "M")
+            {
+                label4.Text = "先 生";
+            }
+            else
+            {
+                label4.Text = "小 姐";
+            }
+
+            pictureBox2.ImageLocation = photo;
+        }
 
     }
     }
