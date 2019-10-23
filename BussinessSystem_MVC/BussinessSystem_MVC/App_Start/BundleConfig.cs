9616b0d72dce869c4b2c49b1674b05afa7e830ce
RoryChang <fuxk280843@gmail.com>
2019-10-23 14:45:11 +0800

    set

---
+++ BussinessSystem_MVC/BussinessSystem_MVC/App_Start/BundleConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace BussinessSystem_MVC.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/Scripts/jquery").Include(
+                "~/Scripts/jquery-{version}.js",
+                "~/Scripts/jquery.*",
+                "~/Scripts/jquery-ui-{version}.js"));          
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/css/bootstrap.css",
+                      "~/css/dashboard.css"));
+
+        }
+    }
+}
---
+++ BussinessSystem_MVC/BussinessSystem_MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BussinessSystem_MVC.MvcApplication" Language="C#" %>
