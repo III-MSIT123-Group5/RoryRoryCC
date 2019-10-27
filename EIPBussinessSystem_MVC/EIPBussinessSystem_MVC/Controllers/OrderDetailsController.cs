b02c304232664b71631883d49035e3c54ca8deef
jenniferwu840209 <jenniferwu840209@yahoo.com.tw>
2019-10-27 14:46:29 +0800

    搜尋功能

--- EIPBussinessSystem_MVC/EIPBussinessSystem_MVC/Controllers/OrderDetailsController.cs
+++ EIPBussinessSystem_MVC/EIPBussinessSystem_MVC/Controllers/OrderDetailsController.cs
@@ -8,45 +8,23 @@ using System.Web;
 using System.Web.Mvc;
 using EIPBussinessSystem_MVC.Models;
 using PagedList;
-//
+
 namespace EIPBussinessSystem_MVC.Controllers
 {
     public class OrderDetailsController : Controller
     {
         private BusinessDataBaseEntities db = new BusinessDataBaseEntities();
         
-        public ActionResult Index(string searching)
+        public ActionResult Index(string searchProductName, string searchOrderID)
         {
-            var ProductName = from OD in db.OrderDetails
+            var report = from OD in db.OrderDetails
                               select OD;
-            if (!String.IsNullOrEmpty(searching))
+            if (!String.IsNullOrEmpty(searchProductName))
             {
-                ProductName = ProductName.Where(s => s.ProductName.Contains(searching));
+                report = report.Where(s => s.ProductName.Contains(searchProductName));
             }
-            return View(ProductName.ToList());
+            return View(report.ToList());         
         }
-
-        //public ActionResult Index(String orderdetailGenre, String SearchString)
-        //{
-        //    var genreList = new List<String>();
-        //    var genreQry = from o in db.OrderDetails
-        //                   orderby o.ProductName
-        //                   select o.ProductName;
-        //    genreList.AddRange(genreQry.Distinct());
-        //    ViewBag.orderdetailGenre = new SelectList(genreList);
-
-        //    var orderdetail = from OD in db.OrderDetails
-        //                      select OD;
-        //    if (!String.IsNullOrEmpty(SearchString))
-        //    {
-        //        orderdetail = orderdetail.Where(s => s.ProductName.Contains(SearchString));
-        //    }
-        //    if (!String.IsNullOrEmpty(orderdetailGenre))
-        //    {
-        //        orderdetail = orderdetail.Where(x => x.ProductName == orderdetailGenre);
-        //    }
-        //    return View(orderdetail);
-        //}
 
         //// GET: OrderDetails
         //public ActionResult Index()
