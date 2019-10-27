aff478570ce9fda9770307de99e7300c0e7c44af
Yongsheng10 <54974398+Yongsheng10@users.noreply.github.com>
2019-10-27 17:29:34 +0800

    update rts

---
+++ EIPBussinessSystem_MVC/EIPBussinessSystem_MVC/Views/ReportTimeSystems/Metadata/ReportTimeSystems.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace EIPBussinessSystem_MVC.Models
+{
+    [MetadataType(typeof(ReportTimeSystemsMetadata))]
+    public partial class ReportTimeSystems 
+    {
+
+    }
+}
---
+++ EIPBussinessSystem_MVC/EIPBussinessSystem_MVC/Views/ReportTimeSystems/Metadata/ReportTimeSystemsMetadata.cs
@@ -0,0 +1,44 @@
+﻿using EIPBussinessSystem_MVC.Views.ReportTimeSystems.Metadata;
+using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace EIPBussinessSystem_MVC.Models
+{
+    public class ReportTimeSystemsMetadata
+    {
+        
+        public int ReportID { get; set; }
+
+        [Required(ErrorMessageResourceName = "ReportNameEmpty", ErrorMessageResourceType =typeof(Resource1),AllowEmptyStrings =false)]
+        [Display(Name ="ReportName",ResourceType = typeof(Resource1))]
+        [StringLength(10,ErrorMessage ="{0}最多{1}個字")]
+        public string ReportName { get; set; }
+
+        [Display(Name = "EmployeeName")]
+        public int employeeID { get;}
+
+        [Required(ErrorMessage = "請輸入{0}")]
+        [Display(Name = "開始時間")]
+        [DisplayFormat(DataFormatString ="{yyyy-MM-dd hh:mm}")]
+        public System.DateTime StartTime { get; set; }
+
+        [Required(ErrorMessage = "請輸入{0}")]
+        [Display(Name = "結束時間")]
+        [DisplayFormat(DataFormatString = "{yyyy-MM-dd hh:mm}")]
+        public System.DateTime EndTime { get; set; }
+
+        [Required(ErrorMessage = "請輸入{0}")]
+        [Display(Name = "結束時間")]
+        [DisplayFormat(DataFormatString = "{2:f}")]
+        public double EventHours { get; set; }
+
+        public int EventID { get; set; }
+        public string Note { get; set; }
+        public System.DateTime ApplyDateTime { get; set; }
+        public Nullable<bool> Discontinue { get; set; }
+
+        public virtual Employee Employee { get; set; }
+        public virtual Event Event { get; set; }
+    }
+}
