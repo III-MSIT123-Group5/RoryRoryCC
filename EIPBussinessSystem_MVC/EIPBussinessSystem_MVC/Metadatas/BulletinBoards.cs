b2845ab54631e64aa5ba5bdbf590ca0e9a064482
jocelyn6065 <55129130+jocelyn6065@users.noreply.github.com>
2019-10-28 12:47:04 +0800

    2019-10-28-2

---
+++ EIPBussinessSystem_MVC/EIPBussinessSystem_MVC/Metadatas/BulletinBoards.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EIPBussinessSystem_MVC.Metadatas
+{
+    [MetadataType(typeof(BulletinBoardsMetadata))]
+    public partial class BulletinBoards
+    {
+    }
+}
