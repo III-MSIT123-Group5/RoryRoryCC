a532aca683c05cb20bf12b729d6ec6a033127eaa
jocelyn6065 <55129130+jocelyn6065@users.noreply.github.com>
2019-10-28 12:56:43 +0800

    2019-10-28-fix-2

--- EIPBussinessSystem_MVC/EIPBussinessSystem_MVC/Metadatas/BulletinBoardsMetadata.cs
+++ EIPBussinessSystem_MVC/EIPBussinessSystem_MVC/Metadatas/BulletinBoardsMetadata.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,11 @@ namespace EIPBussinessSystem_MVC.Metadatas
 {
     public class BulletinBoardsMetadata
     {
-       
+    
+       [Display(Name = "貼文內容")]
+       public string Content { get; set; }
+
+       //[Display(Name = "張貼時間")]
+       //public DateTime PostTime { get; set; }
     }
 }
