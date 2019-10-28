ffeb6c1d6bd9c12cd5b983a202d844a9e99031ff
jocelyn6065 <55129130+jocelyn6065@users.noreply.github.com>
2019-10-28 14:47:47 +0800

    2019-10-28-3

---
+++ EIPBussinessSystem_MVC(new)/EIPBussinessSystem_MVC/ViewModels/CreateBulletinBoardsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EIPBussinessSystem_MVC.ViewModels
+{
+    public class CreateBulletinBoardsViewModel
+    {
+
+        [Display(Name = "貼文內容")]
+        public string Content { get; set; }
+
+        [Display(Name = "發文時間")]
+        public DateTime PostTime { get; set; }
+
+    }
+}
