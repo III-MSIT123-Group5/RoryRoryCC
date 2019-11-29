6ff284e17aac4f89fae08a3c4962deb9d72adde7
jocelyn6065 <55129130+jocelyn6065@users.noreply.github.com>
2019-11-29 16:46:18 +0800

    正常版型 2019-11-29

---
+++ BusinessSystemMVC(Admin page) 2019-11-29/BusinessSystemMVC(Admin page)/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace BusinessSystemMVC_Admin_page_
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
