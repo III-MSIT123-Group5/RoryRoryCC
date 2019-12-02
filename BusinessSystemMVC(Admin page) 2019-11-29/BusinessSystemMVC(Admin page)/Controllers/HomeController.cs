fac068f52bbacfd959148228c2c5cad509acc9cf
RoryChang <fuxk280843@gmail.com>
2019-12-02 12:12:12 +0800

    1202

--- BusinessSystemMVC(Admin page) 2019-11-29/BusinessSystemMVC(Admin page)/Controllers/HomeController.cs
+++ BusinessSystemMVC(Admin page) 2019-11-29/BusinessSystemMVC(Admin page)/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BusinessSystemMVC_Admin_page_.Models;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,8 +10,19 @@ namespace BusinessSystemMVC_Admin_page_.Controllers
 {
     public class HomeController : Controller
     {
+        BusinessDataBaseEntities db = new BusinessDataBaseEntities();
+
         public ActionResult Index()
         {
+            string person = User.Identity.GetUserId();
+            var acc = db.AspNetUsers.Find(person);
+            var q = db.Employees.Where(p => p.Account == acc.UserName).Select(p => p);
+            foreach (var a in q)
+            {
+                EmployeeDetail.Name = a.EmployeeName;
+                EmployeeDetail.EmployeeID = a.employeeID;
+            }
+
             return View();
         }
 
