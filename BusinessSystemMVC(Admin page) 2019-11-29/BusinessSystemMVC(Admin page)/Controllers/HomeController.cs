8868a42e44e65a1f9ed8ac05976fd51281450bcd
jocelyn6065 <55129130+jocelyn6065@users.noreply.github.com>
2019-12-02 12:57:36 +0800

    2019-11-30

--- BusinessSystemMVC(Admin page) 2019-11-29/BusinessSystemMVC(Admin page)/Controllers/HomeController.cs
+++ BusinessSystemMVC(Admin page) 2019-11-29/BusinessSystemMVC(Admin page)/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BusinessSystemMVC_Admin_page_.Models;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +10,8 @@ namespace BusinessSystemMVC_Admin_page_.Controllers
 {
     public class HomeController : Controller
     {
+        private BusinessDataBaseEntities db = new BusinessDataBaseEntities();
+
         public ActionResult Index()
         {
             return View();
@@ -25,6 +29,33 @@ namespace BusinessSystemMVC_Admin_page_.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult GetEmpID()
+        {
+            var userid = User.Identity.GetUserId();
+            var account = db.AspNetUsers.Find(userid);
+
+            var empquery = from em in db.Employees
+                           where em.Account == account.UserName
+                           select new
+                           {
+                               em.employeeID,
+                               em.EmployeeName
+                           };
+
+            //string EmpName;
+            //int EmpID = 0;
+            //foreach (var e in empquery)
+            //{
+            //    EmpID = e.employeeID;
+            //    EmpName = e.EmployeeName;
+            //}
+
+            var datas = empquery.ToList();
+
+            return Json(new {data = datas }, JsonRequestBehavior.AllowGet);
+
         }
     }
 }
