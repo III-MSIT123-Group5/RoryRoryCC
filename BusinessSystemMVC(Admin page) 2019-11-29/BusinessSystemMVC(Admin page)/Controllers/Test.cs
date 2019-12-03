7365b4fa6349a45a774e9ed76a74c98287e9ea7c
kevinsean999 <qwe820321@gmail.com>
2019-12-03 17:06:59 +0800

    kkk

---
+++ BusinessSystemMVC(Admin page) 2019-11-29/BusinessSystemMVC(Admin page)/Controllers/Test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EIPBussinessSystem_MVC.Controllers
+{
+    public class Test
+    {
+        public DateTime dtS { get; set; }
+        public DateTime dtE { get; set; }
+        public string LNid { get; set; }
+        public string purpose { get; set; }
+    }
+}
