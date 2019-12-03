4c3b0df6c2ebab247a165fa06901beff73a219e3
Yvette-Ma <vqahsq489847@gmail.com>
2019-12-03 15:08:50 +0800

    內部訊息

---
+++ BusinessSystemMVC(Admin page) 2019-11-29/BusinessSystemMVC(Admin page)/Controllers/RecipientsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BusinessSystemMVC_Admin_page_.Models;
+
+namespace BusinessSystemMVC_Admin_page_.Controllers
+{
+    public class RecipientsController : Controller
+    {
+        private BusinessDataBaseEntities db = new BusinessDataBaseEntities();
+
+        // GET: Recipients
+        public ActionResult Index()
+        {
+            var recipients = db.Recipients.Include(r => r.Employee).Include(r => r.Message);
+            return View(recipients.ToList());
+        }
+
+        // GET: Recipients/Details/5
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Recipient recipient = db.Recipients.Find(id);
+            if (recipient == null)
+            {
+                return HttpNotFound();
+            }
+            return View(recipient);
+        }
+
+        // GET: Recipients/Create
+        public ActionResult Create()
+        {
+            ViewBag.EmployeeID = new SelectList(db.Employees, "employeeID", "EmployeeName");
+            ViewBag.MessageID = new SelectList(db.Messages, "MessageID", "Data");
+            return View();
+        }
+
+        // POST: Recipients/Create
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MessageID,EmployeeID,Status")] Recipient recipient)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Recipients.Add(recipient);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.EmployeeID = new SelectList(db.Employees, "employeeID", "EmployeeName", recipient.EmployeeID);
+            ViewBag.MessageID = new SelectList(db.Messages, "MessageID", "Data", recipient.MessageID);
+            return View(recipient);
+        }
+
+        // GET: Recipients/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Recipient recipient = db.Recipients.Find(id);
+            if (recipient == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.EmployeeID = new SelectList(db.Employees, "employeeID", "EmployeeName", recipient.EmployeeID);
+            ViewBag.MessageID = new SelectList(db.Messages, "MessageID", "Data", recipient.MessageID);
+            return View(recipient);
+        }
+
+        // POST: Recipients/Edit/5
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MessageID,EmployeeID,Status")] Recipient recipient)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(recipient).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.EmployeeID = new SelectList(db.Employees, "employeeID", "EmployeeName", recipient.EmployeeID);
+            ViewBag.MessageID = new SelectList(db.Messages, "MessageID", "Data", recipient.MessageID);
+            return View(recipient);
+        }
+
+        // GET: Recipients/Delete/5
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Recipient recipient = db.Recipients.Find(id);
+            if (recipient == null)
+            {
+                return HttpNotFound();
+            }
+            return View(recipient);
+        }
+
+        // POST: Recipients/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            Recipient recipient = db.Recipients.Find(id);
+            db.Recipients.Remove(recipient);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
