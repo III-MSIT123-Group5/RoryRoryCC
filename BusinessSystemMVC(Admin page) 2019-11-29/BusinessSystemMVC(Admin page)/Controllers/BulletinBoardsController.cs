6e56d4a425c7cd35dc35ab968fe707ccda16dc16
jocelyn6065 <55129130+jocelyn6065@users.noreply.github.com>
2019-12-16 20:05:47 +0800

    2019-12-16 -2

--- BusinessSystemMVC(Admin page) 2019-11-29/BusinessSystemMVC(Admin page)/Controllers/BulletinBoardsController.cs
+++ BusinessSystemMVC(Admin page) 2019-11-29/BusinessSystemMVC(Admin page)/Controllers/BulletinBoardsController.cs
@@ -98,7 +98,6 @@ namespace BusinessSystemMVC_Admin_page_.Controllers
             {
                 if (b.Num == 0)
                 {
-                    //db.BulletinBoards.Add(b);
 
                     var userid = User.Identity.GetUserId();
                     var account = db.AspNetUsers.Find(userid);
