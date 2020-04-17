ea8fb0c127534f8776ab3cb09885cc498829a8d5
RoryChang <55083193+RoryChang@users.noreply.github.com>
2020-04-17 16:54:39 +0800

    0417

---
+++ LinqLabs (Solution)/LinqLabs/作業/Frm作業_1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LinqLabs.作業
+{
+    public partial class Frm作業_1 : Form
+    {
+        public Frm作業_1()
+        {
+            InitializeComponent();
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            this.lblMaster.Text = "LOG Files";
+
+            System.IO.DirectoryInfo dirs = new System.IO.DirectoryInfo(@"c:\windows");
+            FileInfo[] files = dirs.GetFiles();
+
+            this.dataGridView1.DataSource = files;
+
+        }
+    }
+}
